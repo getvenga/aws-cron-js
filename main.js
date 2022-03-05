ebc769621e25c3fede8e55fa3f2b1d1e8fece0c7
Robert Mercado <rmercado@opentable.com>
2022-03-05 12:52:52 -0500

    finished mock uo

--- main.js
+++ main.js
@@ -7,6 +7,50 @@ const client = new EventBridgeClient({ region: "us-east-1" });
 // prepare AWS commands
 const listRulesCommand = new ListRulesCommand({});
 
+
+const ensureValidTime = (timeValue) => timeValue % 24;
+
+const shiftHour = (hourString, adjustBy) => {
+  let v1;
+  let v2;
+  let joiner;
+
+  if(hourString.includes("/")) {
+    const pieces = hourString.split("/");
+    joiner = "/";
+
+    v1 = pieces[0] === "*" ? "*" : ensureValidTime(Number(pieces[0]) + adjustBy);
+    v2 = pieces[1] === "*" ? "*" : ensureValidTime(Number(pieces[1]) + adjustBy);
+  } else if(hourString.includes("-")) {
+    const pieces = hourString.split("-");
+    joiner = "-";
+
+    v1 = pieces[0] === "*" ? "*" : ensureValidTime(Number(pieces[0]) + adjustBy);
+    v2 = pieces[1] === "*" ? "*" : ensureValidTime(Number(pieces[1]) + adjustBy);
+
+  } else {
+    v1 = hourString === "*" ? "*" : ensureValidTime(Number(hourString) + adjustBy);
+  }
+
+  return [null, undefined].includes(v2) ? `${v1}` : `${v1}${joiner}${v2}`;
+}
+
+const processHourString = (hoursString, adjustBy) => {
+  const shiftedHours = [];
+  if (hoursString === "*") return hoursString;
+
+  if (hoursString.includes(",")) {
+    hoursString.split(",").forEach( hourToken => {
+
+      shiftedHours.push(shiftHour(hourToken, adjustBy));
+    })
+  } else {
+    shiftedHours.push(shiftHour(hoursString, adjustBy));
+  }
+
+  return shiftedHours.join(",");
+}
+
 const processRules = async (rules) => {
   for (const rule of rules) {
       if (rule.ScheduleExpression) {
@@ -19,15 +63,11 @@ const processRules = async (rules) => {
         /*
           what each element means:
           Minutes,	Hours,	Day of month,	Month,	Day of week,	Year */
-        const cronPieces = normalizedExp.split(" ");
-        const minutes = cronPieces[0];
-        const hours = cronPieces[1];
-        const dayOfMonth = cronPieces[2];
-        const dayOfWeek = cronPieces[3];
-        const year = cronPieces[4];
+        const [minutes, hours, dayOfMonth, dayOfWeek, year] = normalizedExp.split(" ");
 
+        const adjusted = processHourString(hours, 1);
 
-        console.log({ full: rule.ScheduleExpression, cronPieces })
+        console.log({ full: rule.ScheduleExpression, adjusted })
 
         //const updateCommand = new PutRuleCommand({ ...rule, ScheduleExpression: 'cron(0 2 ? * MON *)'});
         //const updateResponse = await client.send(updateCommand);
@@ -37,7 +77,9 @@ const processRules = async (rules) => {
 
 try {
   const response = await client.send(listRulesCommand);
-  processRules(response.Rules);
+  await processRules(response.Rules);
+
+  console.log("\nFinished.")
 } catch (err) {
   console.error(err);
 }
